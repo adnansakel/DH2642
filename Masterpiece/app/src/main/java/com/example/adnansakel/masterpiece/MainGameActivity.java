6d2164218c0255eafd91689a9065684ca9413755
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-04-15 18:22:58 +0200

    Added listeners that start game actions, and added Firebase setters

--- Masterpiece/app/src/main/java/com/example/adnansakel/masterpiece/MainGameActivity.java
+++ Masterpiece/app/src/main/java/com/example/adnansakel/masterpiece/MainGameActivity.java
@@ -17,6 +17,10 @@ import com.example.adnansakel.masterpiece.model.MasterpieceGameModel;
 import com.example.adnansakel.masterpiece.model.Painting;
 import com.example.adnansakel.masterpiece.model.Player;
 import com.example.adnansakel.masterpiece.view.MainGameView;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.util.Collections;
 import java.util.List;
@@ -29,6 +33,8 @@ import java.util.Set;
 public class MainGameActivity extends Activity {
 
     MasterpieceGameModel model;
+    //Firebase masterpieceRef;
+    boolean turnIsHappening = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +55,85 @@ public class MainGameActivity extends Activity {
             test.addView(tv);
         }
         */
+
+        /*
         if(AppConstants.IamCreator){
             gameSetUp();
-        }
+        }*/
+
+        //listen for changes in TurnTaker in Firebase
+        Firebase turnTakerRef = new Firebase(AppConstants.GameRef+"/"+"TurnTaker");
+        turnTakerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                System.out.println("TurnTaker changed to: " + snapshot.getValue());
+
+                //if the TurnTaker is me
+                if (snapshot.getValue() == "playerPositionID") {
+                    //TODO: use the right variable to compare to in the above
+                    startTurn();
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+
+        //listen for changes in CurrentBidder in Firebase
+        Firebase currentBidderRef = new Firebase(AppConstants.GameRef+"/"+"CurrentBidder");
+        turnTakerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                System.out.println("CurrentBidder changed to: " + snapshot.getValue());
+
+                //if the CurrentBidder is me
+                if (snapshot.getValue() == "") {
+                    //TODO: use the right variable to compare to in the above
+                    bidOnPainting();
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+
+        //listen for changes in TurnAction in Firebase
+        Firebase turnActionRef = new Firebase(AppConstants.GameRef+"/"+"TurnAction");
+        turnTakerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                System.out.println("TurnAction changed to: " + snapshot.getValue());
+
+                //if a new turn action just started
+                if (snapshot.getValue() != null && turnIsHappening == false) {
+
+                    turnIsHappening = true;
+
+                    //if the turn is a private auction
+                    if (snapshot.getValue() == "privateAuction") {
+
+                        //my device will display the "private auction in progress" screen
+                        //TODO: display the above screen
+                    }
+
+                    //if the turn is a bank auction
+                    if (snapshot.getValue() == "bankAuction") {
+
+                        //my device will display the "bank auction in progress" screen
+                        //TODO: display the above screen
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
     }
 
     /*public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,6 +142,7 @@ public class MainGameActivity extends Activity {
         return true;
     }*/
 
+    /*
     //sets up the game, and is executed only by the game creator
     private void gameSetUp() {
 
@@ -103,7 +186,7 @@ public class MainGameActivity extends Activity {
         Player selectedPlayer = thePlayers.get(index);
         model.setTurnTaker(selectedPlayer);
 
-    }
+    }*/
 
     //executed whenever the turnTaker changes and is equal to my player
     public void startTurn() {
@@ -112,23 +195,34 @@ public class MainGameActivity extends Activity {
         Random rn = new Random();
         int roll = rn.nextInt(1); //there are only 2 types so far
 
+        //get reference to Firebase
+        //masterpieceRef = new Firebase(AppConstants.FireBaseUri);
+
         if(roll == 0){
 
             //PRIVATE AUCTION
 
             //set turn action
-            model.setTurnAction("privateAuction");
+            //model.setTurnAction("privateAuction");
+            new Firebase(AppConstants.GameRef+"/"+"TurnAction").setValue("privateAuction");
 
             //choose painting being auctioned (for now I'm just choosing my player's first painting)
-            Painting chosenPaintingToAuction = model.getMyPlayer().getOwnedPaintings().get(0);
+            //Painting chosenPaintingToAuction = model.getMyPlayer().getOwnedPaintings().get(0);
+            //TODO: need to access my owned paintings
 
             //TODO: allow my player to select a painting by swiping up
 
             //set painting being auctioned
-            model.setPaintingBeingAuctioned(chosenPaintingToAuction);
+            //model.setPaintingBeingAuctioned(chosenPaintingToAuction);
+            new Firebase(AppConstants.GameRef+"/"+"PaintingBeingAuctioned").setValue("");
+            //TODO: set the value of my painting in the above
 
-            //set current bidder as the next person in allPlayers[]
-            model.setCurrentBidder(model.getNextPlayer());
+            //TODO: set my player's bidding property to false in Firebase (eg. It's my auction so I'm done bidding)
+
+            //set current bidder as the next player
+            //model.setCurrentBidder(model.getNextPlayer());
+            //TODO: get the next player
+            new Firebase(AppConstants.GameRef+"/"+"CurrentBidder").setValue("");
 
             //bidding on private auction
 
@@ -138,7 +232,8 @@ public class MainGameActivity extends Activity {
             //BANK AUCTION
 
             //set turn action
-            model.setTurnAction("bankAuction");
+            //model.setTurnAction("bankAuction");
+            new Firebase(AppConstants.GameRef+"/"+"TurnAction").setValue("bankAuction");
 
             //choose painting being auctioned (first painting in Paintings[])
             Painting chosenPaintingToAuction = model.getAllPaintings1().get(0);
@@ -153,6 +248,15 @@ public class MainGameActivity extends Activity {
     }
 
     //executed when currentBidder = myPlayer
+    public void bidOnPainting() {
+
+        //temporarily hide the "private auction in progress" screen or "bank auction in progress" screen
+        //TODO: hide the above screen
+
+        //show the bidding screen
+        //TODO: hide the above screen
+    }
+
     public void Auction() {
         /*
         //full screen popup during auction
