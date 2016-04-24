14172add2d1193489da4aaa533485c8f7f6d4db4
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-04-24 10:51:03 +0200

    End round button visibility

--- Masterpiece/app/src/main/java/com/example/adnansakel/masterpiece/view/MainGameView.java
+++ Masterpiece/app/src/main/java/com/example/adnansakel/masterpiece/view/MainGameView.java
@@ -309,7 +309,7 @@ public class MainGameView implements Observer{
             if(data.toString().equals(AppConstants.WINNERFOUND)){
                 System.out.println("Winner is player "+ model.getWinner());
                 textWinnerName.setText(model.getAllPlayers().get(Integer.valueOf(model.getWinner())).getName());
-                if(model.getWinner().equals(model.getMyPlayer().getPlayerpositionID())){
+                if(model.getWinner().equals(model.getMyPlayer().getPlayerpositionID()+"")){
                     //Make the btn_end_round visible
                     button_end_round.setVisibility(View.VISIBLE);
                 }
